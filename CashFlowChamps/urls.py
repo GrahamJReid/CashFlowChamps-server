af839af2f87f5718a66a60f069bd3d43f2f47dde
GrahamJReid <grahamreidtechnology@gmail.com>
2023-08-01 08:52:16 -0500

    CRUD Expense functional

--- CashFlowChamps/urls.py
+++ CashFlowChamps/urls.py
@@ -18,12 +18,13 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
 from rest_framework import routers
-from cashflowchampsapi.views import register_user, check_user, UserView, BudgetView
+from cashflowchampsapi.views import register_user, check_user, UserView, BudgetView, ExpenseView
 
 
 router = routers.DefaultRouter(trailing_slash=False)
 router.register(r'users', UserView, 'user')
 router.register(r'budgets', BudgetView, 'budget')
+router.register(r'expenses', ExpenseView, 'expense')
 
 
 
