adf342b8e0beef9037dfe4cc53e5788c085041c4
Dro Demby <drotechdemby@gmail.com>
2023-08-01 20:22:50 -0500

    Created Budget_Expense View

--- cashflowchampsapi/views/__init__.py
+++ cashflowchampsapi/views/__init__.py
@@ -2,3 +2,4 @@ from .auth import check_user, register_user
 from .user import UserView
 from .budget_view import BudgetView
 from .expense_view import ExpenseView
+from .budgetExpense import BudgetExpenseView
